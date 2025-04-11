be1eeca10498930a1d0d15927cf699288a388124
Stefan Fries <stefan@freshfries.de>
2025-04-11 11:08:07 +0200

    Added responsible CSS features, header and footer

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import InputForm from './components/InputForm';
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<h1>Restaurant Finder Stefan Fries 👩🏻‍💻</h1>
+		<h1>Restaurant Finder Stefan Fries</h1>
 		<InputForm />
 	</React.StrictMode>
 );
